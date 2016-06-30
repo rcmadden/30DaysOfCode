6a02169d0f9d120951554443eb608dda6d745bbf
Russia Madden <dataengineer007@gmail.com>
2016-06-29 22:40:02 -0700

    solution to week 4 class vs instance

--- hr_04_class_vs_instance.py
+++ hr_04_class_vs_instance.py
@@ -1,22 +1,24 @@
-class Person:
-    def __init__(self,initialAge):
-        # Add some more code to run some checks on initialAge
-        self.initialAge = initialAge
-        if initialAge < 0:
-            self.initialAge = 0
-            print("Age is not valid, setting age to 0")
-        return self.initialAge
+class Person():
+    def __init__(self,initial_Age):
+            # Add some more code to run some checks on initial_Age
+            if initial_Age < 0:
+                print("Age is not valid, setting age to 0.")
+                self.age = 0
+            else:
+                self.age = initial_Age
 
     def amIOld(self):
         # Do some computations in here and print out the correct statement to the console
-        pass
+        if self.age < 13:
+            print("You are young.")
+        elif 13 <= self.age < 18:
+            print("You are a teenager.")
+        else:
+            print("You are old.")
     def yearPasses(self):
         # Increment the age of the person in here
-        pass
+        self.age += 1
 
-
-Cameron = Person(14)
-Cameron.initialAge
 
 t = int(input())
 for i in range(0, t):
